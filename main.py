bb515d2b912d65fe5564ac739bedc81d8fe6a7ab
MiruNyanNyan345 <desmond63870780@gmail.com>
2021-04-30 20:14:55 +0800

    30/04

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import shutil
 import sqlite3
 import time
 import datetime
+import requests
 
 import pandas as pd
 from tqdm import tqdm
@@ -12,15 +13,25 @@ import selenium.common
 from selenium.webdriver.firefox.options import Options
 import selenium.webdriver
 from mozprofile import FirefoxProfile
+import socket
 
 import webslist_crawler
 import analysis
 
 # top_200 = webslist_crawler.getTop200Websites()
 
-top_200 = pd.read_excel("top240.xlsx")
+top_200 = pd.read_excel("top240.xlsx")[:3]
 
 
+# getting geo location of host
+def getHostGeo(web_addr):
+    hostJson = requests.post("http://ip-api.com/batch", json=[
+        {"query": socket.gethostbyname(web_addr)}
+    ]).json()[0]
+    return {"status": hostJson["status"], "country": hostJson["country"], "countryCode": hostJson["countryCode"]}
+
+
+# transfer unix timestamp to timestamp
 def unixToDT(ts_epoch):
     try:
         return True, datetime.datetime.fromtimestamp(ts_epoch)
@@ -61,7 +72,10 @@ def get_cookies(web_addr):
     # print("Website: {}".format(web))
     try:
         driver.get(url=web)
-        selenium_cookies = driver.get_cookies()
+        try:
+            selenium_cookies = driver.get_cookies()
+        except Exception as e:
+            print(e)
 
         profile_repo = glob.glob('/var/folders/7y/kdk1h6ss42j5kb4mc30xdd9w0000gn/T/rust_mozprofile*')
         latest_profile_repo = max(profile_repo, key=os.path.getctime)
@@ -72,8 +86,10 @@ def get_cookies(web_addr):
         db_source = latest_profile_repo + "/cookies.sqlite"
         db_destination = "/tmp/gdpr-cookies-analysis/gdpr-cookies-analysis.default/cookies.sqlite"
 
+        # copy the cookies database from a temporary directory to the web driver profile directory
         shutil.copyfile(db_source, db_destination)
 
+        # connect to the cookies database and extract the cookies
         con = sqlite3.connect(db_destination)
         cur = con.cursor()
         cur.execute("SELECT * FROM moz_cookies")
@@ -95,10 +111,10 @@ def get_cookies(web_addr):
 
         con.close()
 
-        # for cookie in selenium_cookies:
-        #     print("First-Party Cookies ({}): {}".format(web_addr, cookie))
-        # for cookie in sqlite3_cookies:
-        #     print("First-Party Cookies ({}): {}".format(web_addr, cookie))
+        for cookie in selenium_cookies:
+            print("Selenium Cookies ({}): {}".format(web_addr, cookie))
+        for cookie in sqlite3_cookies:
+            print("SQLite Cookies ({}): {}".format(web_addr, cookie))
 
         for item in os.listdir(profile_conf_name):
             if item.endswith(".sqlite"):
@@ -139,12 +155,16 @@ def main():
                                             "httpOnlyGrade",
                                             "secureGrade",
                                             "expiryGrade",
-                                            "avgGrade"])
+                                            "avgGrade",
+                                            "country",
+                                            "countryCode"])
     for web in tqdm(top_200["Root Domain"]):
         print("")
         print(web)
+        # print(getHostGeo(web_addr=web))
         status, results = get_cookies(web)
         if status:
+            hostGeoJson = getHostGeo(web_addr=web)
             for result in results:
                 composeDict = {}
                 # composeDict.update({"website": web, "status": status, "creation": datetime.datetime.now().timestamp()})
@@ -185,16 +205,25 @@ def main():
                                                       "httpOnlyGrade": None,
                                                       "secureGrade": None,
                                                       "expiryGrade": None,
-                                                      "avgGrade": None}, ignore_index=True)
+                                                      "avgGrade": None,
+                                                      "country": None,
+                                                      "countryCode": None}, ignore_index=True)
         else:
             analysis_result = analysis.cookies_analysis(website=web,
                                                         web_rows=top200_cookies_df.loc[
                                                             top200_cookies_df['website'] == web])
             analysis_result.update({"website": web})
+            if hostGeoJson["status"] == "success":
+                analysis_result.update({"country": hostGeoJson["country"]})
+                analysis_result.update({"countryCode": hostGeoJson["countryCode"]})
+            else:
+                analysis_result.update({"country": None})
+                analysis_result.update({"countryCode": None})
+
             top200_grade_df = top200_grade_df.append(analysis_result, ignore_index=True)
 
-    top200_cookies_df.to_csv('top200_cookies.csv', index=False)
-    top200_grade_df.to_csv('top200_grade.csv', index=False)
+    # top200_cookies_df.to_csv('top200_cookies_new.csv', index=False)
+    # top200_grade_df.to_csv('top200_grade_new.csv', index=False)
 
     # with open('top200_cookies.json', 'w') as file:
     #     json.dump(top200_cookies, file)
